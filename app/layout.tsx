c77ae2656b34e60210dec125843206a05e683567
Your Name <milad.bhzz@gmail.com>
2026-01-31 01:18:56 +0330

    update gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 // app/layout.js
 
 import { Inter } from 'next/font/google';
-import Script from 'next/script';
 import './globals.css';
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,33 +12,12 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }: any) {
-  const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
+  const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
 
   return (
     <html lang="en">
       <body className={inter.className}>
-        {GA_ID && (
-          <>
-            <Script
-              strategy="afterInteractive"
-              src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-            />
-            <Script
-              id="google-analytics"
-              strategy="afterInteractive"
-              dangerouslySetInnerHTML={{
-                __html: `
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', '${GA_ID}', {
-                    page_path: window.location.pathname,
-                  });
-                `,
-              }}
-            />
-          </>
-        )}
+        <GoogleTagManager gtmId={GTM_ID || ''} />
         {children}
       </body>
     </html>
