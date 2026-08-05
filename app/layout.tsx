5da70d4b150277668d952be2e1e059c828b2dd00
milad <milad.bhzz@gmail.com>
2026-08-05 18:36:38 +0330

    add hubspot

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { GoogleTagManager } from '@next/third-parties/google'
 import Navigation from './components/Navigation';
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({ children }: any) {
       <body className={inter.className}>
         <GoogleTagManager gtmId={GTM_ID || ''} />
         <Navigation />
+        <Script type="text/javascript" id="hs-script-loader" async defer src="//js-eu1.hs-scripts.com/146914493.js" />
         {children}
       </body>
     </html>
